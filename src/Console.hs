be383fd3b2184e338bef4ffecbd194f7e2c61c64
Călin Ardelean <mmn80cpu@gmail.com>
2016-03-23 13:44:33 +0200

    tweaked command line options

--- src/Console.hs
+++ src/Console.hs
@@ -26,41 +26,47 @@ maybeReader = eitherReader $ \arg ->
 optsParser :: Parser Options
 optsParser = Options
      <$> option maybeReader
-         ( long "type"
+         ( long "doctype"
+        <> short 'd'
         <> value Nothing
         <> metavar "TYPE"
         <> help "Filter by entry type" )
      <*> strOption
-         ( long "key"
+         ( long "citation"
+        <> short 'c'
         <> value ""
         <> metavar "KEY"
         <> help "Filter by citation key" )
      <*> strOption
          ( long "author"
+        <> short 'a'
         <> value ""
         <> metavar "AUTHOR"
         <> help "Filter by author name" )
      <*> strOption
          ( long "title"
+        <> short 't'
         <> value ""
         <> metavar "TITLE"
         <> help "Filter by title" )
      <*> strOption
          ( long "year"
+        <> short 'y'
         <> value ""
         <> metavar "YEAR"
         <> help "Filter by publication year" )
      <*> strOption
-         ( long "tag"
+         ( long "keyword"
+        <> short 'k'
         <> value ""
         <> metavar "TAG"
         <> help "Filter by keyword" )
      <*> option auto
-         ( long "sort"
+         ( long "sortby"
         <> short 's'
         <> value Unsorted
-        <> metavar "SORTBY"
-        <> help "Sort results by title | author | year" )
+        <> metavar "FIELD"
+        <> help "Sort results by FIELD = (title | author | year)" )
      <*> flag Compact BibTeX
          ( long "bibtex"
         <> short 'b'
@@ -73,5 +79,7 @@ main = execParser opts >>= bibline
       ( fullDesc
      <> progDesc "Reads BibTeX on stdin and outputs a possibly filtered and \
                    \sorted list of entries using either BibTeX or a \
-                   \compact human friendly format"
+                   \compact human friendly format. \
+                   \Search strings are case insensitive and wildchars \
+                   \'*' and '?' can be used."
      <> header "bibline - utility for processing BibTeX files" )
